b6a964cd1f3dbfc895bf44f18de8d32906966b13
Sergio Vergara <sergiovergara123@gmail.com>
2024-03-19 09:48:09 -0500

    code was cleaned up

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
   let binary = "";
-  
+
   if (input === 0) {
     binary = "0";
   }
@@ -15,28 +15,6 @@ const decimalToBinary = (input) => {
   }
 
   result.innerText = binary;
-
-  /* const inputs = [];
-  const quotients = [];
-  const remainders = [];
-
-  if (input === 0) {
-    result.innerText = "0";
-    return;
-  }
-
-  while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
-    inputs.push(input);
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient;
-  }
-  console.log("Inputs: ", inputs);
-  console.log("Quotients: ", quotients);
-  console.log("Remainders: ", remainders);
-  result.innerText = remainders.reverse().join(""); */
 };
 
 const checkUserInput = () => {
